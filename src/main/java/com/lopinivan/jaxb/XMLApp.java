ee99a931502737d3ecc68cae465f01179d0960c5
Ivan <xsardesxsardes@gmail.com>
2024-11-28 12:48:27 +0800

    second commit - Added XSD city

--- src/main/java/com/lopinivan/jaxb/XMLApp.java
+++ src/main/java/com/lopinivan/jaxb/XMLApp.java
@@ -19,17 +19,26 @@ public class XMLApp
         person1.setName("Sergey");
         person1.setAge(33);
 
+        City city = new City();
+        city.setCountry("Russia");
+        city.setName("Irkutsk");
+        city.setPeople(700000);
+
         serializationAndDeserializationJavaXML.generateXML(person, "person");
         serializationAndDeserializationJavaXML.generateXML(person1, "person1");
+        serializationAndDeserializationJavaXML.generateXML(city, "city");
 
         Person parcedPerson = new Person();
         Person parcedPerson1 = new Person();
+        City parcedCity = new City();
 
         parcedPerson = serializationAndDeserializationJavaXML.parseXML("person", Person.class);
         parcedPerson1 = serializationAndDeserializationJavaXML.parseXML("person1", Person.class);
+        parcedCity = serializationAndDeserializationJavaXML.parseXML("city", City.class);
 
         System.out.println(parcedPerson.getName() + " " + parcedPerson.getAge());
         System.out.println(parcedPerson1.getName() + " " + parcedPerson1.getAge());
+        System.out.println(parcedCity.getName() + " " + parcedCity.getCountry());
     }
 
 }
